247f25be62042ca6a50ef4a28edbf347053a0e28
Aaren08 <schneiderneil392@gmail.com>
2025-07-03 22:13:04 +0500

    Created the User Profile Info

--- client/src/components/RightSidebar/RightSidebar.jsx
+++ client/src/components/RightSidebar/RightSidebar.jsx
@@ -1,5 +1,53 @@
-const RightSidebar = () => {
-  return <div>RightSidebar</div>;
+import assets, { imagesDummyData } from "../../assets/assets.js";
+import "./RightSidebar.css";
+
+const RightSidebar = ({ selectedUser }) => {
+  return (
+    selectedUser && (
+      <div
+        className={`userProfileContainer ${selectedUser ? "notDisplay" : ""}`}
+      >
+        {/* USER PROFILE */}
+        <div className="userProfileWrapper">
+          <img
+            src={selectedUser?.profilePic || assets.avatar_icon}
+            alt="image"
+            className="userProfileImage"
+          />
+          <h1 className="selectedUserName">
+            <p className="connectionIndicator"></p>
+            {selectedUser?.fullName}
+          </h1>
+
+          {/*  USER BIO */}
+          <p className="userBio">{selectedUser?.bio}</p>
+        </div>
+
+        {/* USER MEDIA */}
+        <hr style={{ borderColor: "#FFFFFF50", margin: "1rem 0" }} />
+        <div className="userMediaWrapper">
+          <p>Media</p>
+          <div className="userMedia">
+            {imagesDummyData.map((url, index) => (
+              <div
+                key={index}
+                style={{
+                  cursor: "pointer",
+                  borderRadius: "0.25rem",
+                }}
+                onClick={() => window.open(url)}
+              >
+                <img src={url} alt="image" className="userMediaImage" />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* LOGOUT */}
+        <button className="logOutBtn">Logout</button>
+      </div>
+    )
+  );
 };
 
 export default RightSidebar;
